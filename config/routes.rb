2f101624958f6d7f57a6ef8763d909a2f9e07374
amccadams <amccadams@gmail.com>
2013-06-22 09:51:55 -0500

    equipment views updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ RailsPhotoAssistant::Application.routes.draw do
 
  
   root to: "home#index"
-  resources :equipments, only: [:list, :index, :new, :show, :create, :edit, :destroy]
+  resources :equipments  
+  # resources :photo_shoot
+  resources :shoots
 
   # devise_for :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130612203743) do
+ActiveRecord::Schema.define(:version => 20130622124704) do
 
   create_table "equipment_lists", :force => true do |t|
     t.integer "equipment_id"
@@ -32,6 +32,21 @@ ActiveRecord::Schema.define(:version => 20130612203743) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "shoots", :force => true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "date"
+    t.string   "time"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|
